7eef9ec69cf3ba8131fe4136dd6c81e2b0f27b4e
JUANES5545 <juanes.reich@gmail.com>
2025-03-16 22:09:19 -0500

    Add better spaces

--- login/app.py
+++ login/app.py
@@ -5,6 +5,7 @@ import datetime
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your_secret_key'
 
+
 @app.route('/generate_token', methods=['POST'])
 def generate_token():
     data = request.get_json()
@@ -15,6 +16,7 @@ def generate_token():
         'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=30)
     }, app.config['SECRET_KEY'], algorithm='HS256')
     return jsonify({'token': token})
+
 
 @app.route('/validate_token', methods=['POST'])
 def validate_token():
@@ -28,6 +30,7 @@ def validate_token():
         return jsonify({'valid': False, 'error': 'Token expired'})
     except jwt.InvalidTokenError:
         return jsonify({'valid': False, 'error': 'Invalid token'})
+
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5020)
