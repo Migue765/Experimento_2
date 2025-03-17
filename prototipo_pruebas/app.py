724d82e8fe95290c94d395a8cb2a8af60f55c60e
Natalia Arandio <a.arandio@uniandes.edu.co>
2025-03-16 20:42:51 -0500

    Se suben micorservicio para prototpipo de pruebas

---
+++ prototipo_pruebas/app.py
@@ -0,0 +1,15 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route("/ping", methods=["GET"])
+def ping():
+    return jsonify({"mensaje": "Pong!"}), 200
+
+@app.route("/update-route", methods=["POST"])
+def procesar_compra():
+    return jsonify({"ruta actualizada correctamente": True}), 200 # 200 es el código de estado HTTP para "OK"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=4043)
